95aa5023aa58d31969c6fd9cd04c40d005dacca4
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2013-05-19 18:11:58 +0200

    Add More aniids

--- resources/GeneratedIDS.cpp
+++ resources/GeneratedIDS.cpp
@@ -122,9 +122,11 @@ GeneratedIDS::FillAniSnd(WriteIDSResource* res)
 
 	res->AddValue(0x6404, "USAR1");
 	res->AddValue(0x7001, "MOGR");
+	res->AddValue(0x7200, "MZOM"); // ZOMBIE
 	res->AddValue(0x7400, "MDOG");
 	res->AddValue(0x7a00, "MSPI");
-	res->AddValue(0x7b01, "MWER"); // WORG ?
+	res->AddValue(0x7b01, "MWLF"); // WOLF_WORG ?
+	res->AddValue(0x7b05, "MWLF"); // WOLF_DREAD ?
 
 	res->AddValue(0x7c01, "MTAS");
 	res->AddValue(0x7e00, "MWER");
