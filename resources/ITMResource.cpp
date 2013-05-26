d6ec9ba4cebac6df7044e66c3f1a4b9ecc1ffdf0
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2013-05-26 19:03:34 +0200

    Fixed ITM resource version checking. Added retrieval of part of header, just to start to get something

--- resources/ITMResource.cpp
+++ resources/ITMResource.cpp
@@ -6,18 +6,29 @@
  */
 
 #include "ITMResource.h"
+#include "MemoryStream.h"
+
+#define ITM_SIGNATURE "ITM "
+#define ITM_VERSION_1 "V1  "
+
+
 
 ITMResource::ITMResource(const res_ref& resName)
 	:
 	Resource(resName, RES_ITM)
 {
-
 }
 
 
 ITMResource::~ITMResource()
 {
+}
 
+
+uint16
+ITMResource::Type() const
+{
+	return fHeader.type;
 }
 
 
@@ -25,5 +36,16 @@ ITMResource::~ITMResource()
 bool
 ITMResource::Load(Archive *archive, uint32 key)
 {
-	return Resource::Load(archive, key);
+	if (!Resource::Load(archive, key))
+		return false;
+
+	if (!CheckSignature(ITM_SIGNATURE))
+		return false;
+
+	if (!CheckVersion(ITM_VERSION_1))
+		return false;
+
+	fData->ReadAt(8, fHeader);
+
+	return true;
 }
--- resources/ITMResource.h
+++ resources/ITMResource.h
@@ -10,12 +10,32 @@
 
 #include "Resource.h"
 
+
+struct itm_header {
+	uint32 name_unidentified;
+	uint32 name_identified;
+	res_ref replacement_item;
+	uint32 flags;
+	uint16 type;
+	char usability[4];
+	char animation[2];
+
+	// TODO: Rest
+};
+
+
 class ITMResource: public Resource {
 public:
 	ITMResource(const res_ref& name);
 	~ITMResource();
 
 	virtual bool Load(Archive *archive, uint32 key);
+
+	uint16 Type() const;
+
+private:
+
+	itm_header fHeader;
 };
 
 #endif /* ITMRESOURCE_H_ */
