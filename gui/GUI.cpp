jackburton79/bgemu

commit 28c2eacc05d4b6d404f2dc138558ca75c2dc5155

Fix windows deletion

gui/GUI.cpp
@@ -137,7 +137,7 @@ GUI::HideWindow(uint16 id)
 	std::vector<Window*>::iterator i;
 	for (i = fActiveWindows.begin(); i != fActiveWindows.end(); i++) {
 		if ((*i)->ID() == id) {
-			fActiveWindows.erase(i);
+			delete *fActiveWindows.erase(i);
 			break;
 		}
 	}
@@ -159,8 +159,11 @@ GUI::IsWindowShown(uint16 id) const
 void
 GUI::Clear()
 {
+	for (std::vector<Window*>::const_iterator i = fActiveWindows.begin();
+			i != fActiveWindows.end(); i++) {
+		delete *i;
+	}
 	fActiveWindows.clear();
-	fActiveWindows.erase(fActiveWindows.begin(), fActiveWindows.end());
 
 	_AddBackgroundWindow();
 }

commit 4530e090d6d78a07fe3f4fa8d64fadd4815b8b60

The iterator bug strikes again. Fixed crash introduced in 28c2eacc05d4b6d404f2dc138558ca75c2dc5155 and delete the windows for real, too.

gui/GUI.cpp
@@ -137,7 +137,8 @@ GUI::HideWindow(uint16 id)
 	std::vector<Window*>::iterator i;
 	for (i = fActiveWindows.begin(); i != fActiveWindows.end(); i++) {
 		if ((*i)->ID() == id) {
-			delete *fActiveWindows.erase(i);
+			delete *i;
+			fActiveWindows.erase(i);
 			break;
 		}
 	}