e75f23b9f502b06777a9269fe8007e80abe4b7e6
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2013-08-28 22:36:24 +0200

    Implement pathfinding using the A* algorithm. Works but hangs if the destination is unreachable.

--- PathFind.cpp
+++ PathFind.cpp
@@ -1,5 +1,15 @@
 #include "PathFind.h"
-#include "Utils.h"
+#include "Room.h"
+
+#include <algorithm>
+#include <limits.h>
+
+struct point_node {
+	IE::point point;
+	int cost;
+	const struct point_node* parent;
+};
+
 
 PathFinder::PathFinder()
 {
@@ -24,44 +34,191 @@ PathFinder::NextWayPoint()
 }
 
 
+/* static */
+bool
+PathFinder::IsPassable(const IE::point& point)
+{
+	int32 state = Room::Get()->PointSearch(point);
+	switch (state) {
+		case 0:
+		case 8:
+		case 10:
+		case 12:
+		case 13:
+			return false;
+		default:
+			return true;
+	}
+}
+
+
 void
 PathFinder::_GeneratePath(const IE::point& start, const IE::point& end)
 {
+	std::cout << std::dec;
+	std::cout << "GeneratePath: (" << start.x << ", " << start.y << ") - (";
+	std::cout << end.x << ", " << end.y << ")" << std::endl;
+
 	fPoints.clear();
 
-	IE::point point = start;
-	int cycle;
-	int lgDelta = end.x - point.x;
-	int shDelta = end.y - point.y;
-	int lgStep = SGN(lgDelta);
-	lgDelta = ABS(lgDelta);
-	int sh_step = SGN(shDelta);
-	shDelta = ABS(shDelta);
-	if (shDelta < lgDelta) {
-		cycle = lgDelta >> 1;
-		while (point.x != end.x) {
-			fPoints.push_back(point);
-			cycle += shDelta;
-			if (cycle > lgDelta) {
-				cycle -= lgDelta;
-				point.y += sh_step;
-			}
-			point.x += lgStep;
-		}
-		fPoints.push_back(point);
-	}
-	cycle = shDelta >> 1;
-	while (point.y != end.y) {
-		fPoints.push_back(point);
-		cycle += lgDelta;
-		if (cycle > shDelta) {
-			cycle -= shDelta;
-			point.x += lgStep;
-		}
-		point.y += sh_step;
+	point_node* node = new point_node;
+	node->point = start;
+	node->parent = NULL;
+	node->cost = 0;
+
+	fOpenList.push_back(node);
+	_AddPassableAdiacentPoints(*node);
+	fOpenList.remove(node);
+	fClosedList.push_back(node);
+
+	for (;;) {
+		point_node* cheapestNode = _ChooseCheapestNode(end);
+		std::cout << "Cheapest Point: (" << cheapestNode->point.x << ", ";
+		std::cout << cheapestNode->point.y << "): cost ";
+		std::cout << cheapestNode->cost << std::endl;
+		fOpenList.remove(cheapestNode);
+		fClosedList.push_back(cheapestNode);
+		if (cheapestNode->point == end || fOpenList.empty())
+			break;
+		_AddPassableAdiacentPoints(*cheapestNode);
 	}
 
-	fPoints.push_back(end);
+	std::list<point_node*>::const_iterator i;
+	for (i = fClosedList.begin(); i != fClosedList.end(); i++) {
+		fPoints.push_back((*i)->point);
+		delete *i;
+	}
+
+	fOpenList.clear();
+	fClosedList.clear();
 
 	fIterator = fPoints.begin();
+}
+
+
+void
+PathFinder::_AddPassableAdiacentPoints(point_node& node)
+{
+	IE::point newPoint = node.point;
+	newPoint.x += 1;
+	_AddIfPassable(newPoint, node);
+	newPoint.y += 1;
+	_AddIfPassable(newPoint, node);
+
+	newPoint = node.point;
+	newPoint.x -= 1;
+	_AddIfPassable(newPoint, node);
+	newPoint.y += 1;
+	_AddIfPassable(newPoint, node);
+
+	newPoint = node.point;
+	newPoint.y += 1;
+	_AddIfPassable(newPoint, node);
+
+	newPoint = node.point;
+	newPoint.y -= 1;
+	_AddIfPassable(newPoint, node);
+	newPoint.x -= 1;
+	_AddIfPassable(newPoint, node);
+
+	newPoint = node.point;
+	newPoint.x += 1;
+	newPoint.y -= 1;
+	_AddIfPassable(newPoint, node);
+}
+
+
+struct FindPoint {
+	FindPoint(const IE::point& point)
+		: toFind(point) {};
+
+	bool operator() (const point_node* node) {
+		return node->point == toFind;
+	};
+	const IE::point& toFind;
+};
+
+
+static inline const uint32
+CalculateCost(const IE::point& pointA, const IE::point& pointB)
+{
+	return (pointA.x == pointB.x)
+			|| (pointA.y == pointB.y) ? 10 : 14;
+}
+
+
+void
+PathFinder::_AddIfPassable(const IE::point& point, point_node& parent)
+{
+	if (point.x < 0 || point.y < 0 || !IsPassable(point))
+		return;
+
+	std::cout << std::dec;
+		std::cout << "point (" << point.x << ", " << point.y << ") ";
+	// Check if point is in closed list
+	std::list<point_node*>::const_iterator iter =
+				std::find_if(fClosedList.begin(), fClosedList.end(),
+							FindPoint(point));
+	if (iter != fClosedList.end()) {
+		std::cout << "already in closed list" << std::endl;
+		return;
+	}
+
+
+	std::list<point_node*>::const_iterator i =
+			std::find_if(fOpenList.begin(), fOpenList.end(),
+						FindPoint(point));
+	if (i != fOpenList.end()) {
+		std::cout << "already in open list" << std::endl;
+		// Point is already on the open list.
+		// Check if getting through the point from this point
+		// is cheaper. If so, set this as parent.
+		const int newCost = CalculateCost(parent.point, (*i)->point) + parent.cost;
+		std::cout << "old cost: " << (*i)->cost << ", new: " << newCost << std::endl;
+		if (newCost < (*i)->cost) {
+			std::cout << "better path, updating cost..." << std::endl;
+			(*i)->parent = &parent;
+			(*i)->cost = newCost;
+		}
+	} else {
+		std::cout << ": adding to open list (cost: ";
+		point_node* newNode = new point_node;
+		const int cost = CalculateCost(point, parent.point);
+		newNode->point = point;
+		newNode->cost = parent.cost + cost;
+		std::cout << newNode->cost << ")" << std::endl;
+		newNode->parent = &parent;
+		fOpenList.push_back(newNode);
+	}
+}
+
+
+point_node*
+PathFinder::_ChooseCheapestNode(const IE::point& end)
+{
+	std::list<point_node*>::const_iterator i;
+	uint32 minCost = UINT_MAX;
+	point_node* result = NULL;
+	for (i = fOpenList.begin(); i != fOpenList.end(); i++) {
+		const point_node* node = *i;
+		uint32 totalCost = _HeuristicDistance(node->point, end) + node->cost;
+		if (totalCost < minCost) {
+			result = *i;
+			minCost = totalCost;
+		}
+	}
+
+	return result;
+}
+
+
+uint32
+PathFinder::_HeuristicDistance(const IE::point& start, const IE::point& end)
+{
+	// Manhattan method
+	int32 distance = (int32)((std::abs(end.x - start.x) + std::abs(end.y - start.y)) * 10);
+
+	std::cout << "Heuristic distance (" << start.x << ", " << start.y << ") -> ";
+	std::cout << "(" << end.x << ", " << end.y << ") = " << distance << std::endl;
+	return distance;
 }
--- PathFind.h
+++ PathFind.h
@@ -4,6 +4,7 @@
 #include "IETypes.h"
 #include "list"
 
+struct point_node;
 class PathFinder {
 public:
 	PathFinder();
@@ -11,10 +12,21 @@ public:
 
 	IE::point NextWayPoint();
 
+	static bool IsPassable(const IE::point& point);
+
 private:
 	std::list<IE::point> fPoints;
 	std::list<IE::point>::iterator fIterator;
 
+	std::list<point_node*> fOpenList;
+	std::list<point_node*> fClosedList;
+
 	void _GeneratePath(const IE::point& start, const IE::point& end);
+	void _AddPassableAdiacentPoints(point_node& node);
+	void _AddIfPassable(const IE::point& point, point_node& node);
+	point_node* _ChooseCheapestNode(const IE::point& end);
+	uint32 _HeuristicDistance(const IE::point& start, const IE::point& end);
+
+	bool _IsPointInOpenList(const IE::point& point);
 };
 #endif // __PATHFIND_H
