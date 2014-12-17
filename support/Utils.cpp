4502b469df3f88e529c60ba1192bbbcbdae9902c
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2014-12-17 10:39:28 +0000

    fopen_case: Simplified a bit

--- support/Utils.cpp
+++ support/Utils.cpp
@@ -40,15 +40,14 @@ fopen_case(const char* filename, const char* flags)
 	assert(filename != NULL);
 	assert(strlen(filename) > 1);
 
-	TPath path(filename, NULL);
+	TPath normalizedFileName(filename, NULL);
 	TPath newPath("/");
 
-	char leaf[PATH_MAX];
-	char* start = (char*)path.Path() + 1;
+	char leaf[NAME_MAX];
+	char* start = (char*)normalizedFileName.Path() + 1;
 	size_t where = 0;
 	while ((where = strcspn(start, "/")) > 0) {
-		char* newStart = start;
-		strncpy(leaf, newStart, where);
+		strncpy(leaf, start, where);
 		leaf[where] = 0;
 
 		DIR* dir = opendir(newPath.Path());
@@ -62,7 +61,7 @@ fopen_case(const char* filename, const char* flags)
 			}
 			closedir(dir);
 		}
-		start = newStart + where + 1;
+		start += where + 1;
 	}
 
 	if (strcasecmp(filename, newPath.Path()))
