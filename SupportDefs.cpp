0966d7f7f21f548d73af2ab40a84b5e586c0a09b
Jackburton79 <stefano.ceccherini@gmail.com>
2016-12-27 14:10:27 +0100

    Add other types size checks

--- SupportDefs.cpp
+++ SupportDefs.cpp
@@ -5,6 +5,12 @@ void
 check_types_size()
 {
 	assert_size(sizeof(int32), 4);
-	assert_size(sizeof(sint32), 4);
 	assert_size(sizeof(uint32), 4);
+	assert_size(sizeof(sint32), 4);
+	assert_size(sizeof(int16), 2);
+	assert_size(sizeof(uint16), 2);
+	assert_size(sizeof(sint16), 2);
+	assert_size(sizeof(int8), 1);
+	assert_size(sizeof(uint8), 1);
+	assert_size(sizeof(sint8), 1);
 }
