3c3496715835bf4cf4b4cad7e6ff8473f3a700ee
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2017-09-13 16:11:43 +0200

    Added a ResourceWindow which shows loaded resources using a ListView (not looking good)

---
+++ custom/ResourceWindow.cpp
@@ -0,0 +1,37 @@
+/*
+ * ResourceWindow.cpp
+ *
+ *  Created on: 17/ago/2017
+ *      Author: stefano
+ */
+
+#include "ListView.h"
+#include "ResManager.h"
+#include "ResourceWindow.h"
+
+ResourceWindow::ResourceWindow()
+	:
+	Window(999, 0, 0, 300, 200, NULL)
+{
+	IE::control* control = new IE::control;
+	control->x = 0;
+	control->y = 0;
+	control->w = 300;
+	control->w = 200;
+	ListView* listView = new ListView(control);
+	StringList resourceList;
+	gResManager->GetCachedResourcesList(resourceList);
+
+	std::vector<std::string>::iterator i;
+	for (i = resourceList.begin(); i != resourceList.end(); i++) {
+		std::string string(*i);
+		listView->AddItem(string);
+	}
+	Add(listView);
+}
+
+
+ResourceWindow::~ResourceWindow()
+{
+}
+
---
+++ custom/ResourceWindow.h
@@ -0,0 +1,19 @@
+/*
+ * ResourceWindow.h
+ *
+ *  Created on: 17/ago/2017
+ *      Author: stefano
+ */
+
+#ifndef _RESOURCEWINDOW_H_
+#define _RESOURCEWINDOW_H_
+
+#include "Window.h"
+
+class ResourceWindow: public Window {
+public:
+	ResourceWindow();
+	~ResourceWindow();
+};
+
+#endif /* _RESOURCEWINDOW_H_ */
