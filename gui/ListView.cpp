2b38722d87fee58bfff9be2ae4cd3ced70e460e7
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2017-09-13 16:11:15 +0200

    Added a ListView control

---
+++ gui/ListView.cpp
@@ -0,0 +1,78 @@
+/*
+ * ListView.cpp
+ *
+ *  Created on: 13/set/2017
+ *      Author: stefano
+ */
+
+#include "Bitmap.h"
+#include "GraphicsEngine.h"
+#include "ListView.h"
+#include "ResManager.h"
+#include "TextSupport.h"
+#include "Window.h"
+
+ListView::ListView(IE::control* control)
+	:
+	Control(control)
+{
+	std::cout << "ListView" << std::endl;
+	fFontResource = gResManager->GetBAM("TOOLFONT");
+	//fBitmap = new Bitmap(fControl->w, fControl->h, 8);
+	fBitmap = new Bitmap(control->w, control->h, 16);
+}
+
+
+ListView::~ListView()
+{
+	fBitmap->Release();
+}
+
+
+void
+ListView::Draw()
+{
+	GFX::rect itemRect = { (sint16)fControl->x, (sint16)fControl->y,
+					50, 20 };
+	for (std::list<std::string>::iterator i = fList.begin();
+			i != fList.end(); i++) {
+		TextSupport::RenderString(*i, fFontResource, 0, fBitmap, itemRect);
+		itemRect.y += 20;
+	}
+
+	GFX::rect destRect(fControl->x, fControl->y, fControl->w, fControl->h);
+	fWindow->ConvertToScreen(destRect);
+	GraphicsEngine::Get()->BlitToScreen(fBitmap, NULL, &destRect);
+}
+
+
+void
+ListView::AttachedToWindow(Window* window)
+{
+	Control::AttachedToWindow(window);
+}
+
+
+void
+ListView::MouseMoved(IE::point point, uint32 transit)
+{
+}
+
+
+void
+ListView::MouseDown(IE::point point)
+{
+}
+
+
+void
+ListView::MouseUp(IE::point point)
+{
+}
+
+
+void
+ListView::AddItem(std::string string)
+{
+	fList.push_back(string);
+}
---
+++ gui/ListView.h
@@ -0,0 +1,36 @@
+/*
+ * ListView.h
+ *
+ *  Created on: 13/set/2017
+ *      Author: stefano
+ */
+
+#ifndef GUI_LISTVIEW_H_
+#define GUI_LISTVIEW_H_
+
+#include <string>
+
+#include "Control.h"
+
+class BAMResource;
+class Bitmap;
+class Window;
+class ListView : public Control {
+public:
+	ListView(IE::control* control);
+	virtual ~ListView();
+
+	virtual void Draw();
+	virtual void AttachedToWindow(Window* window);
+	virtual void MouseMoved(IE::point point, uint32 transit);
+	virtual void MouseDown(IE::point point);
+	virtual void MouseUp(IE::point point);
+
+	void AddItem(std::string string);
+private:
+	BAMResource* fFontResource;
+	Bitmap* fBitmap;
+	StringList fList;
+};
+
+#endif /* GUI_LISTVIEW_H_ */
