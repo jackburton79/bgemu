jackburton79/bgemu

commit 20d086e3c064203046e59b2b3c22c93254365960

Some type fixes. On 64 bit in theory these could be wrong.

archives/BIFArchive.cpp
@@ -172,6 +172,7 @@ BIFArchive::_ExtractFileBlock(Stream &source, Stream &dest)
 
 	uint8 *buffer = NULL;
 	uint8 *destBuffer = NULL;
+	size_t decompressedSize = 0;
 	try {
 		buffer = new uint8[comp];
 		destBuffer = new uint8[decomp];
@@ -182,25 +183,26 @@ BIFArchive::_ExtractFileBlock(Stream &source, Stream &dest)
 		else if ((uint32)read != comp)
 			throw -1;
 
+
 		status_t status = ZLibDecompressor::DecompressBuffer(
-				buffer, comp, destBuffer, decomp);
+				buffer, comp, destBuffer, decompressedSize);
 
 		if (status == 0) {
-			ssize_t write = dest.Write(destBuffer, decomp);
+			ssize_t write = dest.Write(destBuffer, decompressedSize);
 			if (write < 0)
 				throw write;
-			else if ((uint32)write != decomp)
+			else if ((size_t)write != decompressedSize)
 				throw -1;
 		} else
 			throw status;
 	} catch (...) {
-		decomp = (uint32)-1;
+		decompressedSize = (size_t)-1;
 	}
 
 	delete[] buffer;
 	delete[] destBuffer;
 
-	return (ssize_t)decomp;
+	return (ssize_t)decompressedSize;
 }
 
 

resources/BamResource.cpp
@@ -303,16 +303,17 @@ BAMResource::_Load()
 		uint32 len;
 		fData->ReadAt(8, len);
 		uint8 *decompressedData = new uint8[len];
+		size_t decompressedSize = 0;
 		status_t status = ZLibDecompressor::DecompressBuffer(
 							(uint8*)(fData->Data()) + std::ptrdiff_t(12),
-							fData->Size() - 12, decompressedData, len);
+							fData->Size() - 12, decompressedData, decompressedSize);
 
 		if (status != 0) {
 			delete[] decompressedData;
 			throw -1;
 		}
 
-		ReplaceData(new MemoryStream(decompressedData, len, true));
+		ReplaceData(new MemoryStream(decompressedData, decompressedSize, true));
 	}
 
 	// Fall through, since in the previous code block we

SupportDefs.cpp
@@ -13,4 +13,5 @@ check_types_size()
 	assert_size(sizeof(int8), 1);
 	assert_size(sizeof(uint8), 1);
 	assert_size(sizeof(sint8), 1);
+	assert_size(sizeof(char), 1);
 }

resources/KEYResource.h
@@ -6,11 +6,16 @@
 struct KeyFileEntry {
 	KeyFileEntry() { name = NULL; };
 	~KeyFileEntry() { delete[] name; };
+
 	void Dump() const;
 	
 	uint32 length;
 	uint16 location;
 	char* name;
+
+private:
+	KeyFileEntry& operator=(const KeyFileEntry&);
+	KeyFileEntry(const KeyFileEntry&);
 };
 
 

resources/MOSResource.cpp
@@ -51,15 +51,16 @@ MOSResource::Load(Archive* archive, uint32 key)
 		uint32 len;
 		fData->ReadAt(8, len);
 		uint8 *decompressedData = new uint8[len];
+		size_t decompressedSize = 0;
 		status_t status = ZLibDecompressor::DecompressBuffer(
 							(uint8*)fData->Data() + std::ptrdiff_t(12),
-							fData->Size() - 12, decompressedData, len);
+							fData->Size() - 12, decompressedData, decompressedSize);
 		if (status != 0) {
 			delete[] decompressedData;
 			return false;
 		}
 
-		ReplaceData(new MemoryStream(decompressedData, len, true));
+		ReplaceData(new MemoryStream(decompressedData, decompressedSize, true));
 	}
 
 	if (!CheckSignature(MOS_SIGNATURE))

support/ZLibDecompressor.cpp
@@ -25,11 +25,11 @@ ZLibDecompressor::~ZLibDecompressor()
 /* static */
 status_t
 ZLibDecompressor::DecompressBuffer(const void* inputBuffer,
-		const uint32& inputSize, void* outputBuffer,
-		uint32& outputSize)
+		const size_t& inputSize, void* outputBuffer,
+		size_t& outputSize)
 {
-	int status = uncompress((Bytef*)outputBuffer, (uLongf*)&outputSize,
-					(const Bytef*)inputBuffer, (uLong)inputSize);
+	int status = uncompress(static_cast<Bytef*>(outputBuffer), static_cast<uLongf*>(&outputSize),
+							static_cast<const Bytef*>(inputBuffer), static_cast<uLong>(inputSize));
 
 	if (status != Z_OK) {
 		std::cerr << "ZLibDecompressor::DecompressBuffer(): ";

support/ZLibDecompressor.h
@@ -17,9 +17,9 @@ public:
 	~ZLibDecompressor();
 
 	static status_t DecompressBuffer(const void* inputBuffer,
-								const uint32& inputSize,
+								const size_t& inputSize,
 								void* outputBuffer,
-								uint32& outputSize);
+								size_t& outputSize);
 };
 
 #endif /* ZLIBDECOMPRESSOR_H_ */

commit ca3967b2ba8dbabb5f1bd1752e94e7b6fa776087

Fix decompressing resources which I broke in 20d086e3c064203046e59b2b3c22c93254365960

archives/BIFArchive.cpp
@@ -172,7 +172,7 @@ BIFArchive::_ExtractFileBlock(Stream &source, Stream &dest)
 
 	uint8 *buffer = NULL;
 	uint8 *destBuffer = NULL;
-	size_t decompressedSize = 0;
+	size_t decompressedSize = decomp;
 	try {
 		buffer = new uint8[comp];
 		destBuffer = new uint8[decomp];

resources/BamResource.cpp
@@ -303,7 +303,7 @@ BAMResource::_Load()
 		uint32 len;
 		fData->ReadAt(8, len);
 		uint8 *decompressedData = new uint8[len];
-		size_t decompressedSize = 0;
+		size_t decompressedSize = len;
 		status_t status = ZLibDecompressor::DecompressBuffer(
 							(uint8*)(fData->Data()) + std::ptrdiff_t(12),
 							fData->Size() - 12, decompressedData, decompressedSize);