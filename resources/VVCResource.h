d77ad23e7cc599a39dda5453b7c57dc0ee9efbe7
JackBurton79 <stefano.ceccherini@gmail.com>
2020-06-08 18:37:29 +0200

    added VVCResource header

---
+++ resources/VVCResource.h
@@ -0,0 +1,51 @@
+#ifndef __VVCRESOURCE_H
+#define __VVCRESOURCE_H
+
+#include "Resource.h"
+
+#include <map>
+
+namespace GFX {
+	struct Palette;
+}
+
+class Bitmap;
+class VVCResource : public Resource {
+public:
+	VVCResource(const res_ref& name);
+	
+	virtual bool Load(Archive *archive, uint32 key);
+
+	virtual void Dump();
+
+/*	Bitmap* FrameForCycle(uint8 cycleIndex, uint16 frameIndex);
+	
+	uint16 CountFrames() const;
+	uint16 CountFrames(uint8 cycleIndex) const;
+	uint8 CountCycles() const;
+
+	uint8 TransparentIndex() const;
+
+	void PrintFrames(uint8 cycleIndex) const;
+	void DumpFrames(const char *path);
+*/
+private:
+	virtual ~VVCResource();
+/*	void _Load();
+	uint8 _FindTransparentIndex();
+
+	Bitmap* _FrameAt(uint16 index);
+
+	GFX::Palette *fPalette;
+	uint32 fFramesOffset;
+	uint32 fCyclesOffset;
+	uint32 fFrameLookupOffset;
+	
+	uint16 fNumFrames;
+	uint8 fNumCycles;
+	uint8 fCompressedIndex;
+	uint8 fTransparentIndex;*/
+};
+
+
+#endif
