bdbd7b89430258074d1108a2a5ed6d08e7b36a94
JackBurton79 <stefano.ceccherini@gmail.com>
2020-12-12 19:57:08 +0100

    Cleanup

--- support/Utils.cpp
+++ support/Utils.cpp
@@ -47,23 +47,23 @@ fopen_case(const char* filename, const char* flags)
 	Path normalizedFileName(filename);
 	std::string newPath("/");
 	char* start = (char*)normalizedFileName.String() + 1;
-	char *end = start + strlen(normalizedFileName.String());
+	char* end = start + ::strlen(normalizedFileName.String());
 	size_t where = 0;
-	while ((where = strcspn(start, "/")) > 0) {
+	while ((where = ::strcspn(start, "/")) > 0) {
 		std::string leaf;
 		leaf.append(start, where);
-		DIR* dir = opendir(newPath.c_str());
+		DIR* dir = ::opendir(newPath.c_str());
 		if (dir != NULL) {
 			dirent *entry = NULL;
-			while ((entry = readdir(dir)) != NULL) {
-				if (!strcasecmp(entry->d_name, leaf.c_str())) {
+			while ((entry = ::readdir(dir)) != NULL) {
+				if (!::strcasecmp(entry->d_name, leaf.c_str())) {
 					if (newPath != "/")
 						newPath.append("/");
 					newPath.append(entry->d_name);
 					break;
 				}
 			}
-			closedir(dir);
+			::closedir(dir);
 		}
 		start += where + 1;
 		if (start >= end)
@@ -71,8 +71,8 @@ fopen_case(const char* filename, const char* flags)
 	}
 
 	FILE* handle = NULL;
-	if (!strcasecmp(filename, newPath.c_str()))
-		handle = fopen(newPath.c_str(), flags);
+	if (!::strcasecmp(filename, newPath.c_str()))
+		handle = ::fopen(newPath.c_str(), flags);
 
 	return handle;
 }
@@ -84,7 +84,7 @@ leaf(const char *path)
 	if (path == NULL)
 		return NULL;
 
-	const char* leafPointer = strrchr(path, '/');
+	const char* leafPointer = ::strrchr(path, '/');
 	return leafPointer != NULL ? leafPointer : path;
 }
 
@@ -95,7 +95,7 @@ extension(const char* path)
 	if (path == NULL)
 		return NULL;
 
-	const char* point = path + strlen(path) - 4;
+	const char* point = path + ::strlen(path) - 4;
 	if (point == NULL || *point != '.')
 		return NULL;
 	return point + 1;
@@ -110,7 +110,7 @@ unquote_string(char* dest, char* source, size_t length)
 	if (length < 0)
 		length = 0;
 	if (length >= 0) {
-		strncpy(dest, source + 1, length);
+		::strncpy(dest, source + 1, length);
 		dest[length] = '\0';
 	}
 	return length;
