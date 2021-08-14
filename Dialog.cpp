1b53610a551413559272a631ff872074417b025e
JackBurton79 <stefano.ceccherini@gmail.com>
2021-08-14 11:42:07 +0200

    DialogHandler: Improve debug output

--- Dialog.cpp
+++ Dialog.cpp
@@ -45,9 +45,13 @@ DialogHandler::GetNextValidState()
 		if (fState == NULL)
 			break;
 		std::vector<trigger_node*> triggerList = Parser::TriggersFromString(fState->Trigger());
-		std::cout << "GetNextValidState: Checking triggers..." << std::endl;
+		std::cout << "GetNextValidState: Checking triggers... " ;
+		if (triggerList.size() == 0) {
+			std::cout << "no trigger found." << std::endl;
+			break;
+		}
 		if (Actor()->EvaluateDialogTriggers(triggerList)) {
-			std::cout << "GetNextValidState(): a trigger returned true!" << std::endl;
+			std::cout << "a trigger returned true!" << std::endl;
 			break;
 		}
 	}
