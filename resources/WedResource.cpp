9a956eb6607e686fef20259cb3e323284de744c4
JackBurton79 <stefano.ceccherini@gmail.com>
2021-09-03 17:46:21 +0200

    Fix crash introduced in 891dd909c987c1bf4cdb322e1972d24444f4973e

--- resources/WedResource.cpp
+++ resources/WedResource.cpp
@@ -142,7 +142,7 @@ WEDResource::GetOverlay(uint32 index)
 
 	MapOverlay* mapOverlay = new MapOverlay(overlay.width, overlay.height, overlay.resource_ref);
 	const uint32 tileMapsCount = overlay.height * overlay.width;
-	for (uint32 tileMapIndex = 0; index < tileMapsCount; tileMapIndex++) {
+	for (uint32 tileMapIndex = 0; tileMapIndex < tileMapsCount; tileMapIndex++) {
 		TileMap* tileMap = _ReadTileMap(overlay.tilemap_offset,
 										overlay.tile_lookup_offset,
 										tileMapIndex);
