9535867500bd19ed9ee1823e142bb2ffffa686e1
Jackburton79 <stefano.ceccherini@gmail.com>
2022-08-29 22:43:11 +0200

    Remove unused method

--- PathFind.cpp
+++ PathFind.cpp
@@ -102,8 +102,6 @@ private:
 			const IE::point& goal) const;
 	point_node* _GetCheapestNode() const;
 	void _ReconstructPath(point_node* goal);
-
-	IE::point _CreateDirectPath(const IE::point&, const IE::point& point);
 };
 
 
@@ -446,53 +444,4 @@ PathFinderImpl::_ReconstructPath(point_node* goal)
 		fPoints->push_front(walkNode->point);
 		walkNode = const_cast<point_node*>(walkNode->parent);
 	}
-}
-
-
-IE::point
-PathFinderImpl::_CreateDirectPath(const IE::point& start, const IE::point& end)
-{
-	IE::point point = start;
-	int cycle;
-	int lgDelta = end.x - point.x;
-	int shDelta = end.y - point.y;
-	int lgStep = std::signbit((float)lgDelta) ? -fStep : fStep;
-	lgDelta = std::abs(lgDelta);
-	int shStep = std::signbit((float)shDelta) ? -fStep : fStep;
-	shDelta = std::abs(shDelta);
-	if (shDelta < lgDelta) {
-		cycle = lgDelta >> 1;
-		while (std::abs(point.x - end.x) > fStep) {
-			if (!_IsPassable(point))
-				return fPoints->back();
-			fPoints->push_back(point);
-			cycle += shDelta;
-			if (cycle > lgDelta) {
-				cycle -= lgDelta;
-				point.y += shStep;
-			}
-			point.x += lgStep;
-		}
-		if (!_IsPassable(point))
-			return fPoints->back();
-
-		fPoints->push_back(point);
-	}
-	cycle = shDelta >> 1;
-	while (std::abs(point.y - end.y) > fStep) {
-		if (!_IsPassable(point))
-			return fPoints->back();
-		fPoints->push_back(point);
-		cycle += lgDelta;
-		if (cycle > shDelta) {
-			cycle -= shDelta;
-			point.x += lgStep;
-		}
-		point.y += shStep;
-	}
-
-	if (_IsPassable(end))
-		fPoints->push_back(end);
-
-	return fPoints->back();
 }
