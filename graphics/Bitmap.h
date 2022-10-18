6a0a7b3b778de6171b787483ca63577008dfe025
Jackburton79 <stefano.ceccherini@gmail.com>
2022-10-18 17:57:33 +0200

    Reorder methods, added comments

--- graphics/Bitmap.h
+++ graphics/Bitmap.h
@@ -25,11 +25,16 @@ class Polygon;
 class Bitmap : public AutoDeletingReferenceable {
 public:
 	Bitmap(uint16 width, uint16 height, uint16 bytesPerPixel);
-	
 	void ImportData(const void *data, uint32 width, uint32 height);
+
+	// Locking
+	bool Lock() const;
+	void Unlock() const;
+
 	void* Pixels() const;
 	uint32 GetPixel(uint16 x, uint16 y) const;
 
+	// Attributes
 	GFX::rect Frame() const;
 	uint16 Width() const;
 	uint16 Height() const;
@@ -39,16 +44,20 @@ public:
 	void SetPosition(uint16 x, uint16 y);
 
 	void Clear(uint32 color = 0);
+
+	// Colors and palette
 	void SetColors(GFX::Color* colors, uint8 start, int num);
 	void SetColors(const GFX::Color& color, uint8 start, int num);
 	void GetPalette(GFX::Palette& palette) const;
 	void SetPalette(const GFX::Palette& palette);
+
 	bool GetColorKey(uint32& colorKey) const;
 	void SetColorKey(uint32 key, bool on = true);
 	void SetColorKey(uint8 r, uint8 g, uint8 b, bool on = true);
 	void ClearColorKey() { SetColorKey(0, false); };
 	void SetAlpha(uint8 alphaValue, bool on = true);
 
+	// Drawing
 	void PutPixel(int32 x, int32 y, const uint32 color);
 	void StrokeLine(int32 xStart, int32 yStart,
 			int32 xEnd, int32 yEnd, const uint32 color);
@@ -69,15 +78,14 @@ public:
 
 	uint32 MapColor(const uint8 r, const uint8 g, const uint8 b);
 
+	// Flip / Mirror
 	Bitmap* GetMirrored() const;
 	void Mirror();
 	void Flip();
 
-	bool Lock() const;
-	void Unlock() const;
-
 	Bitmap* Clone() const;
 
+	// Load / Save
 	static Bitmap* Load(const char* fileName);
 	static Bitmap* Load(const void* data, const size_t size);
 
