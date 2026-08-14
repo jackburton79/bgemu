4092475577c38f30b7403e9de29d09d519f89af0
JackBurton79 <stefano.ceccherini@gmail.com>
2026-08-15 00:10:10 +0200

    More tests for opcode4

--- game/MovieDecoder.h
+++ game/MovieDecoder.h
@@ -63,6 +63,7 @@ private:
 	void TestExecute(uint8 opcode, const uint8 data[], uint32 size, const char* name = NULL);
 	void TestFinish(Bitmap* bitmap, const uint8 expectedResult[], uint32 dataSize);
 	void TestOpcode4A();
+	void TestOpcode4B();
 	void TestOpcode7A();
 	void TestOpcode7B();
 	void TestOpcode8A();
