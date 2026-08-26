f67d4e1e0e5d74be8f4652d29f2bf8c57f1f0c34
JackBurton79 <stefano.ceccherini@gmail.com>
2026-08-26 10:27:59 +0200

    Added more debug output in ResourceManager destructor temporarily comment dialog resource deletion since it's crashing on quit

--- game/ResManager.cpp
+++ game/ResManager.cpp
@@ -113,12 +113,23 @@ ResourceManager::~ResourceManager()
 	gResManager->ReleaseResource(sAniSnd);
 	gResManager->ReleaseResource(sAnimate);
 	gResManager->ReleaseResource(sAlignment);
-	gResManager->ReleaseResource(sDialogs);
+	//gResManager->ReleaseResource(sDialogs);
 
+
+	std::cout << kComponentName << "Cached resources...";
+	std::cout << std::endl;
+	for (auto resource: fCachedResources) {
+			//if (fDebugLevel > 0) {
+			std::cout << "resource: " << resource.second->Name();
+			std::cout << "(" << strresource(resource.second->Type()) << ")";
+			std::cout << " (refcount = " << resource.second->RefCount() << ")..." << std::endl;
+			//}
+	}
 	std::cout << kComponentName << "Deleting cached resources...";
 	std::cout << std::endl;
 	for (auto resource: fCachedResources) {
 		//if (fDebugLevel > 0) {
+			std::cout << "resource: " << "0x" << std::hex << (char*)(resource.second) << std::endl;
 			std::cout << "Deleting " << resource.second->Name();
 			std::cout << "(" << strresource(resource.second->Type()) << ")";
 			std::cout << " (refcount = " << resource.second->RefCount() << ")..." << std::endl;
