6442ddacd100c2e7fda434d35ffda70757f7db17
JackBurton79 <stefano.ceccherini@gmail.com>
2026-09-09 16:42:28 +0200

    actor: print time_intervals and removal_timer

--- game/IETypes.cpp
+++ game/IETypes.cpp
@@ -477,6 +477,8 @@ actor::Print() const
 	std::cout << "\tanimation: " << IDTable::AnimationAt(animation);
 	std::cout << "(" << animation << ")" << std::endl;
 	std::cout << "\torientation: " << orientation << std::endl;
+	std::cout << "\tremoval_timer; " << removal_timer << std::endl;
+	std::cout << "\ttime_intervals; " << time_intervals << std::endl;
 	std::cout << "\tdialog: " << dialog << std::endl;
 	std::cout << "\tcre attached: " << ((flags & IE::ACTOR_CRE_EXTERNAL) ? "NO" : "YES") << std::endl;
 
